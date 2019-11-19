f371003764a327eb0014d262169b0edc04a8b706
ntdat2000 <ntdat18@apcs.vn>
2019-11-19 09:01:57 +0700

    SplashCScreen

---
+++ FrontEnd/SplashScreen.cpp
@@ -0,0 +1,94 @@
+#include "SplashScreen.h"
+
+void SplashScreen(int x)
+{
+
+	
+	cout << "\t\t\t\t";
+	textcolor(14);
+	cout << "  RRRRRRRRRRRRRRRRR        OOOOOOOOO                 AAA               DDDDDDDDDDDDD        " << endl;
+	cout << "\t\t\t\t";
+	cout << "  R::::::::::::::::R     OO:::::::::OO              A:::A              D::::::::::::DDD     "<< endl;
+	cout << "\t\t\t\t";
+	cout << "  R::::::RRRRRR:::::R  OO:::::::::::::OO           A:::::A             D:::::::::::::::DD   "<< endl;
+	cout << "\t\t\t\t";
+	cout << "  RR:::::R     R:::::RO:::::::OOO:::::::O         A:::::::A            DDD:::::DDDDD:::::D  "<< endl;
+	cout << "\t\t\t\t";
+	cout << "    R::::R     R:::::RO::::::O   O::::::O        A:::::::::A             D:::::D    D:::::D "<< endl;
+	cout << "\t\t\t\t";
+	cout << "    R::::R     R:::::RO:::::O     O:::::O       A:::::A:::::A            D:::::D     D:::::D"<< endl;
+	cout << "\t\t\t\t";
+	cout << "    R::::RRRRRR:::::R O:::::O     O:::::O      A:::::A A:::::A           D:::::D     D:::::D"<< endl;
+	cout << "\t\t\t\t";
+	cout << "    R:::::::::::::RR  O:::::O     O:::::O     A:::::A   A:::::A          D:::::D     D:::::D"<< endl;
+	cout << "\t\t\t\t";
+	cout << "    R::::RRRRRR:::::R O:::::O     O:::::O    A:::::A     A:::::A         D:::::D     D:::::D"<< endl;
+	cout << "\t\t\t\t";
+	cout << "    R::::R     R:::::RO:::::O     O:::::O   A:::::AAAAAAAAA:::::A        D:::::D     D:::::D"<< endl;
+	cout << "\t\t\t\t";
+	cout << "    R::::R     R:::::RO:::::O     O:::::O  A:::::::::::::::::::::A       D:::::D     D:::::D"<< endl;
+	cout << "\t\t\t\t";
+	cout << "    R::::R     R:::::RO::::::O   O::::::O A:::::AAAAAAAAAAAAA:::::A      D:::::D    D:::::D "<< endl;
+	cout << "\t\t\t\t";
+	cout << "  RR:::::R     R:::::RO:::::::OOO:::::::OA:::::A             A:::::A   DDD:::::DDDDD:::::D  "<< endl;
+	cout << "\t\t\t\t";
+	cout << "  R::::::R     R:::::R OO:::::::::::::OOA:::::A               A:::::A  D:::::::::::::::DD   "<< endl;
+	cout << "\t\t\t\t";
+	cout << "  R::::::R     R:::::R   OO:::::::::OO A:::::A                 A:::::A D::::::::::::DDD     "<< endl;
+	cout << "\t\t\t\t";
+	cout << "  RRRRRRRR     RRRRRRR     OOOOOOOOO  AAAAAAA                   AAAAAAADDDDDDDDDDDDD  " <<endl;
+	
+	cout << endl << endl;
+
+	textcolor(12);
+	cout << "          CCCCCCCCCCCCCRRRRRRRRRRRRRRRRR        OOOOOOOOO        SSSSSSSSSSSSSSS    SSSSSSSSSSSSSSS IIIIIIIIIINNNNNNNN        NNNNNNNN        GGGGGGGGGGGGG" << endl;
+	cout << "       CCC::::::::::::CR::::::::::::::::R     OO:::::::::OO    SS:::::::::::::::S SS:::::::::::::::SI::::::::IN:::::::N       N::::::N     GGG::::::::::::G" << endl;
+	cout << "     CC:::::::::::::::CR::::::RRRRRR:::::R  OO:::::::::::::OO S:::::SSSSSS::::::SS:::::SSSSSS::::::SI::::::::IN::::::::N      N::::::N   GG:::::::::::::::G" << endl;
+	cout << "    C:::::CCCCCCCC::::CRR:::::R     R:::::RO:::::::OOO:::::::OS:::::S     SSSSSSSS:::::S     SSSSSSSII::::::IIN:::::::::N     N::::::N  G:::::GGGGGGGG::::G" << endl;
+	cout << "   C:::::C       CCCCCC  R::::R     R:::::RO::::::O   O::::::OS:::::S            S:::::S              I::::I  N::::::::::N    N::::::N G:::::G       GGGGGG" << endl;
+	cout << "  C:::::C                R::::R     R:::::RO:::::O     O:::::OS:::::S            S:::::S              I::::I  N:::::::::::N   N::::::NG:::::G              " << endl;
+	cout << "  C:::::C                R::::RRRRRR:::::R O:::::O     O:::::O S::::SSSS          S::::SSSS           I::::I  N:::::::N::::N  N::::::NG:::::G              " << endl;
+	cout << "  C:::::C                R:::::::::::::RR  O:::::O     O:::::O  SS::::::SSSSS      SS::::::SSSSS      I::::I  N::::::N N::::N N::::::NG:::::G    GGGGGGGGGG" << endl;
+	cout << "  C:::::C                R::::RRRRRR:::::R O:::::O     O:::::O    SSS::::::::SS      SSS::::::::SS    I::::I  N::::::N  N::::N:::::::NG:::::G    G::::::::G" << endl;
+	cout << "  C:::::C                R::::R     R:::::RO:::::O     O:::::O       SSSSSS::::S        SSSSSS::::S   I::::I  N::::::N   N:::::::::::NG:::::G    GGGGG::::G" << endl;
+	cout << "  C:::::C                R::::R     R:::::RO:::::O     O:::::O            S:::::S            S:::::S  I::::I  N::::::N    N::::::::::NG:::::G        G::::G" << endl;
+	cout << "   C:::::C       CCCCCC  R::::R     R:::::RO::::::O   O::::::O            S:::::S            S:::::S  I::::I  N::::::N     N:::::::::N G:::::G       G::::G" << endl;
+	cout << "    C:::::CCCCCCCC::::CRR:::::R     R:::::RO:::::::OOO:::::::OSSSSSSS     S:::::SSSSSSSS     S:::::SII::::::IIN::::::N      N::::::::N  G:::::GGGGGGGG::::G" << endl;
+	cout << "     CC:::::::::::::::CR::::::R     R:::::R OO:::::::::::::OO S::::::SSSSSS:::::SS::::::SSSSSS:::::SI::::::::IN::::::N       N:::::::N   GG:::::::::::::::G" << endl;
+	cout << "       CCC::::::::::::CR::::::R     R:::::R   OO:::::::::OO   S:::::::::::::::SS S:::::::::::::::SS I::::::::IN::::::N        N::::::N     GGG::::::GGG:::G" << endl;
+	cout << "          CCCCCCCCCCCCCRRRRRRRR     RRRRRRR     OOOOOOOOO      SSSSSSSSSSSSSSS    SSSSSSSSSSSSSSS   IIIIIIIIIINNNNNNNN         NNNNNNN        GGGGGG   GGGG" << endl;
+
+                
+	 
+	textcolor(10);
+	//cout << "WELCOME                 
+	cout << "\n\n";
+	cout << "\t\t\t\t\t\t\t";
+	cout << "     WELCOME TO GROUP THE SHADOW GAME" << endl;
+	cout << "\n\n\n\n\n";
+
+	//cout << "LOADING
+	cout << "\t\t\t\t\t\t\t\t";
+	cout << "         Loading..." << endl;
+	cout << "\t\t\t\t\t\t   ";
+	for (int i = 0; i < 50; i++)
+	{
+		cout << char(178);
+	}
+	cout << "\r\t\t\t\t\t\t   ";
+	for (int i = 0; i < 50; i++)
+	{
+		cout << char(219);
+		Sleep(30);
+	}
+	cout << "\n";
+	cout << "\t\t\t\t\t\t\t     ";
+
+	//cout << "END
+	system("pause");
+	system("cls");
+
+
+
+
+}
---
+++ FrontEnd/SplashScreen.h
@@ -0,0 +1,9 @@
+#ifndef _SPLASHSCREEN_H_
+#define _SPLASHCREEN_H_
+
+#include <iostream>
+#include "helper.h"
+using namespace std;
+
+void SplashScreen(int x);
+#endif 

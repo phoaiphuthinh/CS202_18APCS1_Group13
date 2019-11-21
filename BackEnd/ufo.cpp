1ea4934ae4bb3b679b03a617e7fa7fdc3ee7d72b
hieu <hmhieu18@apcs.vn>
2019-11-21 19:45:21 +0700

    add class ufo

---
+++ BackEnd/ufo.cpp
@@ -0,0 +1,25 @@
+
+ufo::ufo()
+{
+	h = 20; //size of image
+	w = 20; //size of image
+}
+
+ufo::ufo(int x, int y)
+{
+	mY = y;
+	mX = x; 
+	h = 20; //size of image
+	w = 20; //size of image
+}
+
+void ufo::draw()
+{
+	//Coboloxi's work
+}
+
+void ufo::makeSound()
+{
+	PlaySound(TEXT("ufoCrash.wav"), NULL, SND_ASYNC);
+}
+
---
+++ BackEnd/ufo.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "vehicle.h"
+
+class ufo : public Vehicle {
+private:
+public:
+	ufo();
+	ufo(int x, int y);
+	~ufo();
+	void draw();
+	void makeSound();
+};

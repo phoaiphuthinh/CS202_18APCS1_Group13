bf2fe46a49ae06bd025cdff1c104169112b2a17b
ntdat2000 <ntdat18@apcs.vn>
2019-11-21 23:47:44 +0700

    cat and car

---
+++ FrontEnd/shape.cpp
@@ -0,0 +1,54 @@
+#include "shape.h"
+
+void car()
+{
+//cout<<"         ___________           "<<endl;
+//cout<<"        /     |     \\         "<<endl;
+//cout<<"    ___/______|______\\_____    "<<endl;
+//cout<<"   (   ___    |- ___       \\   "<<endl;
+//cout<<"    \\_|/   \\|__|/   \\]0=##=0]  "<<endl;
+//cout<<"     \\___//     \\___/  \\__//   "<<endl;  
+cout<<"    .------;-.          "<<endl;
+cout<<" __/ , { ) |__;.__    "<<endl;
+cout<<"|    __     :  __ `.  "<<endl;
+cout<<" '--(__)----(__)--'   "<<endl;
+}
+
+void duck()
+{
+        //cout<<"              .:+ooo+/.                   "<<endl;
+        //cout<<"            +hyo++++oshh+`                "<<endl;
+        //cout<<"           yy+++//++/++oyd`               "<<endl;
+        //cout<<"          :m++++//++//osohs..:+-          "<<endl;
+        //cout<<"          -N++/+//++/+yhodhyssm/          "<<endl;
+        //cout<<"           od++/++++++++oNyoyh/           "<<endl;
+        //cout<<" `hs:-...-:+dms+++++++++hdshmo`           "<<endl;
+        //cout<<" -Nosyyyssooosso++//+++++hd:/o:           "<<endl;
+        //cout<<" `mo+++ohhhyyssyyy+/++///+m.              "<<endl;
+        //cout<<"  :mo++sms+++/////+////+++m.              "<<endl;
+        //cout<<"   -hy+++yhhyyyyhhs+///++yy               "<<endl;
+        //cout<<"     /hyo++++oo++++//++ohs`               "<<endl;
+        //cout<<"      -oyys++/++//+oyhs.                  "<<endl;
+        //cout<<"          `-+ossssso/-                    "<<endl;
+	//     ___
+    //    (  ')<,  
+	//	 /  .  )
+	//  (__~___)
+}
+
+void cat()
+{
+//cout<<"   /\\___/\\                         "<<endl;
+//cout<<"  (  \\  .-""-.   A.-.A       "<<endl;
+//cout<<"   \\  \\/    \\_/ , , \\   "<<endl;
+//cout<<"    \\   \\   =;   t  /=    "<<endl;
+//cout<<"     \\   |"".    ',--'       "<<endl;
+//cout<<"      / //   |  ||            "<<endl;
+//cout<<"     /_,))   |_,))            "<<endl;
+	cout << "  /\\___/\\  " << endl;
+	cout << " (  '\\-/') " << endl;
+	cout << " (    .  ) " << endl;
+	cout << "  \\__/\\__/ " << endl;
+
+}
+
---
+++ FrontEnd/shape.h
@@ -0,0 +1,10 @@
+#ifndef _SHAPE_H_
+#define _SHAPE_H_
+
+#include <iostream>
+using namespace std;
+
+void car();
+void duck();
+void cat();
+#endif 
